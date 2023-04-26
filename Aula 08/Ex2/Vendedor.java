5dc854f71724b0eb2b486ff34d529135f1a2b6c2
luckmigz <lucas.matosnegri@hotmail.com>
2023-04-26 16:26:23 -0300

    Update 26/04/23

--- Aula 08/Ex2/Vendedor.java
+++ Aula 08/Ex2/Vendedor.java
@@ -1,3 +1,20 @@
-public class Vendedor {
-    
+public class Vendedor extends  Empregado {
+    private int produtos;
+    private double salario;
+
+    public Vendedor(String nome ,String sobrenome,String cpf, double salario, int produtos){
+        super(nome, sobrenome, cpf);
+        this.salario = salario;
+        this.produtos = produtos;
+    }
+    public void setProdutos(int produtos){
+        this.produtos = produtos;
+    }
+    public int getProduto(){
+        return produtos;
+    }
+    public double salario(){
+        salario = salario*produtos;
+        return salario;
+    }
 }
