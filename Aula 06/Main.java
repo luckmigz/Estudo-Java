a5fd9b212c8d153eb3b8624ffe1eec895c1320c2
luckmigz <lucas.matosnegri@hotmail.com>
2023-05-01 14:43:50 -0300

    Update 01/05/23

---
+++ Aula 06/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        int[] a = {1,3,99,32,1990,2001,00001,1000};
+        System.out.println("Antes do Sort");
+        for (int i: a){
+            System.err.print(i+" ");
+        }
+        System.out.println("");
+        Sort sort = new Sort();
+        sort.selectionSort(a);
+        System.out.println("Após o Sort");
+        for (int i: a){
+            System.err.print(i+" ");
+        }
+    }
+}
