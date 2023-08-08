34abef236ae638ad545c27f6dd3cb98fcd682be3
luckmigz <lucas.matosnegri@hotmail.com>
2023-08-08 17:01:30 -0300

    Front terminado?

--- LoginScreen.java
+++ LoginScreen.java
@@ -4,24 +4,43 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class LoginScreen extends JFrame {
-      
-
+    private JPanel panel = new JPanel(new FlowLayout());
+    private JButton button = new JButton("Confirma");
+    private JLabel label2 = new JLabel("Senha");
+    private JLabel label = new JLabel("Usuario");
+    private JTextField tf = new JTextField( " " );
+    private JPasswordField passwordField = new JPasswordField( "" );
+    String user = " ", pass = " ";
     public LoginScreen(){
-        this.setTitle("Login");
-        this.setSize(400, 300);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-        this.setResizable(false);
-        this.setLocationRelativeTo(null);
-        this.setLayout(new GridLayout(2,3));
-        JPanel panel = new JPanel();
-        this.add(panel,CENTER_ALIGNMENT);
-        JButton confirm = new JButton("Confirma"); 
-        panel.add(confirm,BOTTOM_ALIGNMENT);
-        JPasswordField field = new JAsswordField(); 
-        JTextField tf = new JTextField("Digite o nome de usuario");
-        
-
+    super( "Login" );
+      setLayout( new GridLayout(3,2) ); // configura layout do frame
+      add(label, BorderLayout.EAST );
+      add( tf, BorderLayout.EAST ); 
+      add( label2, BorderLayout.EAST);
+      add( passwordField ); 
+      panel.add( button );
+      add(panel, BorderLayout.SOUTH);
+      TextFieldHandler handler = new TextFieldHandler();
+      button.addActionListener(handler);
 
     }
+   
+    private class TextFieldHandler implements ActionListener{
+        public void actionPerformed( ActionEvent event ){
+
+            if( event.getSource() == button){   
+                user = tf.getText();
+                pass = String.format( new String (passwordField.getPassword()));
+                System.out.println("Click");
+                
+               
+            }
+        }
+    }
+    public String getPassword(){
+         return pass;
+       }
+       public String getUser(){
+            return user;
+       }
 }
--- Logintest.java
+++ Logintest.java
@@ -1,6 +1,13 @@
+import javax.swing.JFrame;
+
 public class Logintest {
     public static void main(String[] args) {
         LoginScreen log = new LoginScreen();
-        
+        log.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        log.setSize( 325, 125 );
+        log.setVisible(true);
+        log.setResizable(false);
+        System.out.println(log.getPassword());
+        System.out.println(log.getUser()); 
     }
 }
